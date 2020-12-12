ac5db6ad1d5d7bc4826f89b38f6be8966fe4678e
gunwooa <gunwoooa@naver.com>
2020-12-13 01:56:24 +0900

    - 4.17 function 컴포넌트, memo 정리 :)

--- src/components/habitAddForm.jsx
+++ src/components/habitAddForm.jsx
@@ -1,31 +1,29 @@
-import React, { PureComponent } from 'react';
+import React, { memo } from 'react';
 
+const HabitAddForm = memo((props) => { // memo : Class형 컴포넌트의 PureComponent 와 같은 역할
+  const formRef = React.createRef();
+  const inputRef = React.createRef(); // vue의 v-model 같은 느낌
 
-class HabitAddForm extends PureComponent {
-  formRef = React.createRef();
-  inputRef = React.createRef(); // vue의 v-model 같은 느낌
-
-  onSubmit = (event) => {
+  const onSubmit = (event) => {
     event.preventDefault(); // 브라우저 리로드 막아줌
-    const name = this.inputRef.current.value;
-    name && this.props.onAdd(name); // 이름이 존재하면
-    // this.inputRef.current.value = ``; // null: O, undefined: X
-    this.formRef.current.reset();
+    const name = inputRef.current.value;
+    name && props.onAdd(name); // 이름이 존재하면
+    // inputRef.current.value = ``; // null: O, undefined: X
+    formRef.current.reset();
   };
-  render() {
-    console.log(`habitAddForm`)
-    return (
-      <form ref={this.formRef} className="add-form" onSubmit={this.onSubmit}>
-        <input
-          ref={this.inputRef}
-          type="text"
-          className="add-input"
-          placeholder="습관을 입력하세요!"
-        />
-        <button className="add-button">Add</button>
-      </form>
-    );
-  }
-}
+
+  return (
+    <form ref={formRef} className="add-form" onSubmit={onSubmit}>
+      <input
+        ref={inputRef}
+        type="text"
+        className="add-input"
+        placeholder="습관을 입력하세요!"
+      />
+      <button className="add-button">Add</button>
+    </form>
+  );
+});
+
  
 export default HabitAddForm;
